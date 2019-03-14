2428c0abb8c998bcf7df3955bd07ae718634ff08
Jessica Olivieri <jessicaolivieri@jessicas-macbook-pro.local>
2019-03-14 11:02:46 -0400

    1.4: Insurance options added (not yet linked to rest of app)

---
+++ Prescription Promise 1-1/InsuranceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InsuranceViewController.swift
+//  Prescription Promise 1-1
+//
+//  Created by Jessica Olivieri on 3/14/19.
+//  Copyright © 2019 Jessica Olivieri. All rights reserved.
+//
+
+import UIKit
+
+class InsuranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+	
+	@IBAction func medicareButtonPressed(_ sender: UIButton) {
+	}
+	@IBAction func medicaidButtonPressed(_ sender: UIButton) {
+	}
+	@IBAction func privateButtonPressed(_ sender: UIButton) {
+	}
+	@IBAction func noneButtonPressed(_ sender: UIButton) {
+	}
+	
+}
